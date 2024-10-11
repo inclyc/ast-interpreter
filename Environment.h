4e1b1817ff83fda298843c15cd4b477e1e18bb52
Yingchi Long <longyingchi24s@ict.ac.cn>
2024-10-11 19:35:04 +0800

    Environment: split

--- Environment.h
+++ Environment.h
@@ -65,90 +65,24 @@ class Environment {
 
    FunctionDecl * mEntry;
 public:
-   /// Get the declartions to the built-in functions
-   Environment() : mStack(), mFree(NULL), mMalloc(NULL), mInput(NULL), mOutput(NULL), mEntry(NULL) {
-   }
+  /// Get the declartions to the built-in functions
+  Environment();
 
+  /// Initialize the Environment
+  void init(TranslationUnitDecl *unit);
 
-   /// Initialize the Environment
-   void init(TranslationUnitDecl * unit) {
-	   for (TranslationUnitDecl::decl_iterator i =unit->decls_begin(), e = unit->decls_end(); i != e; ++ i) {
-		   if (FunctionDecl * fdecl = dyn_cast<FunctionDecl>(*i) ) {
-			   if (fdecl->getName().equals("FREE")) mFree = fdecl;
-			   else if (fdecl->getName().equals("MALLOC")) mMalloc = fdecl;
-			   else if (fdecl->getName().equals("GET")) mInput = fdecl;
-			   else if (fdecl->getName().equals("PRINT")) mOutput = fdecl;
-			   else if (fdecl->getName().equals("main")) mEntry = fdecl;
-		   }
-	   }
-	   mStack.push_back(StackFrame());
-   }
+  FunctionDecl *getEntry();
 
-   FunctionDecl * getEntry() {
-	   return mEntry;
-   }
+  /// !TODO Support comparison operation
+  void binop(BinaryOperator *bop);
 
-   /// !TODO Support comparison operation
-   void binop(BinaryOperator *bop) {
-	   Expr * left = bop->getLHS();
-	   Expr * right = bop->getRHS();
+  void decl(DeclStmt *declstmt);
+  void declref(DeclRefExpr *declref);
 
-	   if (bop->isAssignmentOp()) {
-		   int val = mStack.back().getStmtVal(right);
-		   mStack.back().bindStmt(left, val);
-		   if (DeclRefExpr * declexpr = dyn_cast<DeclRefExpr>(left)) {
-			   Decl * decl = declexpr->getFoundDecl();
-			   mStack.back().bindDecl(decl, val);
-		   }
-	   }
-   }
+  void cast(CastExpr *castexpr);
 
-   void decl(DeclStmt * declstmt) {
-	   for (DeclStmt::decl_iterator it = declstmt->decl_begin(), ie = declstmt->decl_end();
-			   it != ie; ++ it) {
-		   Decl * decl = *it;
-		   if (VarDecl * vardecl = dyn_cast<VarDecl>(decl)) {
-			   mStack.back().bindDecl(vardecl, 0);
-		   }
-	   }
-   }
-   void declref(DeclRefExpr * declref) {
-	   mStack.back().setPC(declref);
-	   if (declref->getType()->isIntegerType()) {
-		   Decl* decl = declref->getFoundDecl();
-
-		   int val = mStack.back().getDeclVal(decl);
-		   mStack.back().bindStmt(declref, val);
-	   }
-   }
-
-   void cast(CastExpr * castexpr) {
-	   mStack.back().setPC(castexpr);
-	   if (castexpr->getType()->isIntegerType()) {
-		   Expr * expr = castexpr->getSubExpr();
-		   int val = mStack.back().getStmtVal(expr);
-		   mStack.back().bindStmt(castexpr, val );
-	   }
-   }
-
-   /// !TODO Support Function Call
-   void call(CallExpr * callexpr) {
-	   mStack.back().setPC(callexpr);
-	   int val = 0;
-	   FunctionDecl * callee = callexpr->getDirectCallee();
-	   if (callee == mInput) {
-		  llvm::errs() << "Please Input an Integer Value : ";
-		  scanf("%d", &val);
-
-		  mStack.back().bindStmt(callexpr, val);
-	   } else if (callee == mOutput) {
-		   Expr * decl = callexpr->getArg(0);
-		   val = mStack.back().getStmtVal(decl);
-		   llvm::errs() << val;
-	   } else {
-		   /// You could add your code here for Function call Return
-	   }
-   }
+  /// !TODO Support Function Call
+  void call(CallExpr *callexpr);
 };
 
 
